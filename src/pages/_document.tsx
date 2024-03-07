9511b48ce282f881713d67751ba03a4671877b7a
zaki-app <matchnokaori@gmail.com>
2024-03-07 22:39:45 +0900

    レイアウト修正

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,19 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Footer from '@/components/organisms/Footer';
+import Header from '@/components/organisms/Header';
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='ja'>
+      <Head>
+        <title>Zaki-dev</title>
+      </Head>
       <body>
-        <Main />
+        <Header />
+        <div className='w-full h-screen'>
+          <Main />
+        </div>
+        <Footer />
         <NextScript />
       </body>
     </Html>
