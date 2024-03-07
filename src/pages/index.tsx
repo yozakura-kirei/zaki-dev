a98ee55a9cbec367da749d53f671228941fc92e7
zaki-app <matchnokaori@gmail.com>
2024-03-07 22:03:31 +0900

    husky導入

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,36 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
-export default function Page({name}: {name: string}) {
+export default function Page({ name }: { name: string }) {
   return (
     <main>
       <p>新しいアプリ {name}</p>
       <p>{process.env.APP_ENDPONT}</p>
+      <p>https://github.com/vercel/next.js/discussions/59731</p>
+      <p>👆これ読んでみて</p>
     </main>
   );
 }
 
 export async function getServerSideProps() {
-  const res = await fetch(`${process.env.APP_ENDPONT}/api/hello`)
-  console.log(res)
+  const res = await fetch(`${process.env.APP_ENDPONT}/api/hello`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+  console.log(res);
   let response;
   try {
     if (res.ok) {
       response = await res.json();
-      console.log("レスポンスOK", response)
-    } 
-  } catch(err) {
-    console.error("why error...", err)
-    response = { name: "Api call Error..."}
+      console.log('レスポンスOK', response);
+    }
+  } catch (err) {
+    console.error('why error...', err);
+    response = { name: 'Api call Error...' };
   }
 
-  return {props: response}
+  return { props: response };
 }
