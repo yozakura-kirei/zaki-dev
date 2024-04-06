fefc0db6bc7e24b449bca7efe2299df6cfdd7aa4
zaki-app <matchnokaori@gmail.com>
2024-04-06 21:01:25 +0900

    feat: ZD-12 gtag追加

--- src/components/templates/PageWrapper.tsx
+++ src/components/templates/PageWrapper.tsx
@@ -1,6 +1,8 @@
-import { ReactNode } from 'react';
+import { ReactNode, Suspense } from 'react';
 import Header from '../organisms/Header';
 import Footer from '../organisms/Footer';
+// import Gtag from '../atoms/Gtag';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 interface PageWrapperProps {
   children: ReactNode;
@@ -8,24 +10,15 @@ interface PageWrapperProps {
 }
 
 export default function PageWrapper({ children, isGrid }: PageWrapperProps) {
+  console.log(process.env.NEXT_PUBLIC_GA_ID);
   return (
     <>
       <Header />
+      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID as string} />
       <main className='mt-[4rem] max-w-[1024px] min-h-screen m-auto px-6 py-6 bg-BackGround text-neutral-800'>
         {children}
       </main>
       <Footer />
     </>
-    // <>
-    //   {isGrid ? (
-    //     <main className='mt-[4rem] max-w-[1024px] h-screen m-auto px-6 pt-6 pb-[5rem] bg-BackGround text-neutral-800'>
-    //       {children}
-    //     </main>
-    //   ) : (
-    //     <main className='mt-[4rem] max-w-[1024px] m-auto p-6 h-screen bg-BackGround text-neutral-800'>
-    //       {children}
-    //     </main>
-    //   )}
-    // </>
   );
 }
